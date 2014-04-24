baa52c2699504e01d2740663e816fd7e947cb575
shuaiwei <weishuai0716@yahoo.com>
2014-04-24 16:49:30 -0400

    create kernel.sh

---
+++ linuxKerDev/syscallSignals/kernel.sh
@@ -0,0 +1,7 @@
+cd init_get_linux_3.2.54/
+mv RW /usr/src/linux/
+mv Makefile /usr/src/linux/Makefile
+mv sched.h /usr/src/linux/include/linux/sched.h 
+mv signal.c /usr/src/linux/kernel/signal.c
+mv unistd_64.h /usr/src/linux/arch/x86/include/asm/unistd_64.h 
+mv unistd_64.h_user /usr/include/x86_64-linux-gnu/asm/unistd_64.h
